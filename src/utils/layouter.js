b6605797615114275a701dd1ec0ac57b50313bf3
Hanks <zhang.han113@zte.com.cn>
2016-05-06 11:00:32 +0800

    实现求集合对称差的功能

--- src/utils/layouter.js
+++ src/utils/layouter.js
@@ -4,10 +4,18 @@ function isEqual(pa, pb) {
     return pa.x === pb.x && pa.y === pb.y;
 }
 
+// 求集合对称差，参考公式：AΔB = (A - B)∪(B - A)
+function xor(A, B) {
+    return []
+        .concat(A.filter(a => B.every(b => !isEqual(a, b))))
+        .concat(B.filter(b => A.every(a => !isEqual(a, b))))
+}
+
 function computePosition(gridWidth, boxes) {
 }
 
 module.exports = {
     isEqual,
+    xor,
     computePosition,
 }
