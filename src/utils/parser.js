f0e7910fc7d61a1575a71c378888e7a1dfc777a1
Hanks <zhang.han113@zte.com.cn>
2016-05-06 14:35:49 +0800

    实现添加网格边框的功能

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,7 +1,7 @@
 
 // 根据属性值计算布局样式
 function getGridStyle(props, origins) {
-    const { unit = 'px', column, width, layout, gap = 0 } = props;
+    const { unit = 'px', column, width, layout, gap = 0, border } = props;
     const ratio = (width + gap) / column;
 
     // 计算 Grid 的高度
@@ -18,13 +18,24 @@ function getGridStyle(props, origins) {
     const layoutStyle = layout.map((size, index) => {
         const coord = origins[index];
 
-        return {
+        const boxStyle = {
             position: 'absolute',
+            boxSizing: 'border-box',
             top   : coord.y * ratio + unit,
             left  : coord.x * ratio + unit,
             width : size[0] * ratio - gap + unit,
             height: size[1] * ratio - gap + unit,
-        };
+        }
+
+        // 给网格添加边框
+        if (border) {
+            const { width = '1px', style = 'solid', color = '#000' } = border;
+            const borderStyle = `${width} ${style} ${color}`;
+            if (coord.x > 0) boxStyle.borderLeft = borderStyle;
+            if (coord.y > 0) boxStyle.borderTop  = borderStyle;
+        }
+
+        return boxStyle;
     });
 
     return { wrapperStyle, layoutStyle };
