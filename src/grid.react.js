c93421aa1a04f3059506a2b805f19ac2b2114ad9
Hanks <zhang.han113@zte.com.cn>
2016-05-06 16:19:28 +0800

    实现 React 版本的布局组件

--- src/grid.react.js
+++ src/grid.react.js
@@ -1,2 +1,28 @@
+// const React = require('react');
 const { computePosition } = require('./utils/layouter');
 const { getGridStyle } = require('./utils/parser');
+
+// 简化 createElement 变量名
+const h = React.createElement;
+
+function GridLayout(props) {
+    const origins = computePosition(props);
+    const { wrapperStyle, layoutStyle } = getGridStyle(props, origins);
+
+    return h(
+        'div',
+        { className: 'grid-wrapper', style: wrapperStyle },
+        React.Children.map(props.children, (cell, index) => h(
+            'div',
+            {
+                className: 'grid-cell',
+                'data-index': index,
+                style: layoutStyle[index]
+            },
+            cell
+        ))
+    )
+}
+
+// window.GridLayout = GridLayout;
+module.exports = GridLayout;
