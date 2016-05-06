f79c81c240908bfb6f540bd3864e191b14892f91
Hanks <zhang.han113@zte.com.cn>
2016-05-06 11:16:55 +0800

    实现计算点集位置信息的功能

--- src/utils/layouter.js
+++ src/utils/layouter.js
@@ -33,7 +33,36 @@ function getOrigin(boundary) {
     return { x: minX, y: minY };
 }
 
+// =========================================================
+//  计算每个图形应该摆放的起始位置
+//  @param 1: 网格横向总宽度
+//  @param 2: 由每个图形的宽高构成的数组 [{x:*,y:*},...]
+//  返回值: 每个图形应该摆放的起始位置所构成的数组
+// =========================================================
 function computePosition(gridWidth, boxes) {
+    const record = [{ x: 0, y: 0 }];       // 初始化所有格子的起点记录
+
+    // 初始化起点和底部边界线
+    let origin = { x: 0, y: 0 };
+    let boundary = [origin, { x: gridWidth, y: 0 }];
+
+    boxes.forEach(box => {
+
+        // 计算新的 boundary
+        boundary = xor(boundary, [
+            move(origin, 0, 0),
+            move(origin, box.x, 0),
+            move(origin, 0, box.y),
+            move(origin, box.x, box.y)
+        ]);
+
+        // 计算下一个起点的位置
+        origin = getOrigin(boundary);
+
+        record.push(origin);
+    });
+
+    return record;
 }
 
 module.exports = {
