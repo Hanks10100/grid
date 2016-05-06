795019c635edfc78d03bcc2eabd1bc5f1a002733
Hanks <zhang.han113@zte.com.cn>
2016-05-06 11:36:37 +0800

    封装 jQuery / Zepto 插件

--- src/grid.jquery.js
+++ src/grid.jquery.js
@@ -1,2 +1,25 @@
 const { computePosition } = require('./utils/layouter');
 const { parseElementParam, getGridStyle } = require('./utils/parser');
+
+// 扩展 jQuery / Zepto 插件
+$.fn.extend({
+    applyGrid: function(options) {
+        // TODO: 添加校验
+        options = options || parseElementParam();
+        const { wrapperStyle, layoutStyle } = getGridStyle(options);
+
+        this.each((x, wrapper) => {
+            $(wrapper)
+                .addClass('grid-wrapper')
+                .css(wrapperStyle)
+                .children().each((index, cell) => {
+                    $(cell).wrap(
+                        $('<div></div>')
+                            .addClass('grid-cell')
+                            .attr('cell-index', index)
+                            .css(layoutStyle[index])
+                    );
+                });
+        });
+    }
+});
