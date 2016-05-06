19c74f31737cf06c551edd8ab676adf84c683488
Hanks <zhang.han113@zte.com.cn>
2016-05-06 10:59:42 +0800

    实现判断点是否相等的功能

--- src/utils/layouter.js
+++ src/utils/layouter.js
@@ -1,7 +1,13 @@
 
+// 判断两点是否相等
+function isEqual(pa, pb) {
+    return pa.x === pb.x && pa.y === pb.y;
+}
+
 function computePosition(gridWidth, boxes) {
 }
 
 module.exports = {
+    isEqual,
     computePosition,
 }
