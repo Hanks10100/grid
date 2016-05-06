3c46ddde3f4f3c7dc344f3a15ba24323bed11d6e
Hanks <zhang.han113@zte.com.cn>
2016-05-06 14:26:44 +0800

    使支持 gap 功能

--- src/utils/parser.js
+++ src/utils/parser.js
@@ -1,11 +1,11 @@
 
 // 根据属性值计算布局样式
 function getGridStyle(props, origins) {
-    const { unit = 'px', column, width, layout } = props;
-    const ratio = width / column;
+    const { unit = 'px', column, width, layout, gap = 0 } = props;
+    const ratio = (width + gap) / column;
 
     // 计算 Grid 的高度
-    const height = origins.pop().y * ratio;
+    const height = origins.pop().y * ratio - gap;
 
     // 计算外框样式
     const wrapperStyle = {
@@ -22,8 +22,8 @@ function getGridStyle(props, origins) {
             position: 'absolute',
             top   : coord.y * ratio + unit,
             left  : coord.x * ratio + unit,
-            width : size[0] * ratio + unit,
-            height: size[1] * ratio + unit,
+            width : size[0] * ratio - gap + unit,
+            height: size[1] * ratio - gap + unit,
         };
     });
 
